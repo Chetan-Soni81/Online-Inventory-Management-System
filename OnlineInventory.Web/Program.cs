c622dcca40bc91c477c0ed07e5eeed39c6a33dc8
Chetan-Soni81 <cs8840744109@gmail.com>
2024-03-12 12:42:17 +0530

    updated

--- OnlineInventory.Web/Program.cs
+++ OnlineInventory.Web/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("MSIConnection"))
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 builder.Services.AddTransient<IRoleRepository, RoleRepository>();
